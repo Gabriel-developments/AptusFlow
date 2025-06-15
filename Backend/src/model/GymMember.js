a6a92608817970c637579a11ccaf5d1cbc72419d
Gabriel-developments <GabrielTheDeveloperOFC@gmail.com>
2025-06-14 23:18:03 -0300

    Criação de ModelSchema de personal

--- Backend/src/model/GymMember.js
+++ Backend/src/model/GymMember.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const GymMemberSchema = new mongoose.Schema(
     {
         name: {type: String, require: true},
+        email: {type: String, require: true},
         password: {type: String, require: true},
         objective: {type: String, require: true},
         hasLimitingConditions: {type: Boolean, require: true},
